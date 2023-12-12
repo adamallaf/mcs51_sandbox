d17ddca38a85ab4139e66df9e2117d1b4f4868f6
Adam Allaf <adam.allaf@hotmail.com>
2023-12-12 03:59:36 +0100

    Added test in python for uart example

---
+++ uart/test_uart.py
@@ -0,0 +1,26 @@
+"""
+A simple uart test, the MCU must send back what it recieves.
+../stcgal-pyenv/bin/python test_uart.py
+"""
+from serial import Serial
+from time import sleep
+
+
+if __name__ == "__main__":
+    print("Press ctrl+c to close")
+    s = Serial(port="/dev/ttyUSB0", baudrate=115200, timeout=1)
+    try:
+        while True:
+            s.write(b'Hello')
+            b = s.read(5)
+            if b:
+                assert b == b'Hello'
+                print(f"sent and received {b.decode()[:-1]}", end='\r')
+                time.sleep(1)
+            else:
+                print("didn't receive a response!")
+                break
+    finally:
+        s.close()
+        print()
+        print("closed")
