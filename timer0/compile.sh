17466f1301cd10d2c08669b40ab9ccefc1b1e081
Adam Allaf <adam.allaf@hotmail.com>
2023-12-12 04:25:45 +0100

    Updated timer0 compilation script

--- timer0/compile.sh
+++ timer0/compile.sh
@@ -1,2 +1,4 @@
-sdcc -mmcs51 timer0.c || exit
-packihx timer0.ihx > timer0.hex
+mkdir -p build/
+rm -vrf build/timer0.*
+(cd build && sdcc -mmcs51 ../timer0.c || exit)
+packihx build/timer0.ihx > timer0.hex
