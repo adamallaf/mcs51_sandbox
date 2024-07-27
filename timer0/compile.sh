e12d9495d196cd7bfc245d1b34a571372a59ec5a
Adam Allaf <adam.allaf@hotmail.com>
2024-07-27 16:43:30 +0200

    Reformatted timer0

--- timer0/compile.sh
+++ timer0/compile.sh
@@ -1,4 +1,4 @@
 mkdir -p build/
 rm -vrf build/timer0.*
 (cd build && sdcc -mmcs51 ../timer0.c || exit)
-packihx build/timer0.ihx > timer0.hex
+packihx build/timer0.ihx >timer0.hex
--- timer0/timer0.c
+++ timer0/timer0.c
@@ -1,12 +1,11 @@
 #include <mcs51reg.h>
 
-
 volatile unsigned char ticks = 0;
 
 /*
  * this delay is not working for values > 255,
  * the asm code below shows how sdcc compiler
- * is dealing with "while(ticks < ms)" when 
+ * is dealing with "while(ticks < ms)" when
  * ticks is unsigned int:
 ;	timer0.c:8: while(ticks < ms);
 00101$:
@@ -20,49 +19,53 @@ volatile unsigned char ticks = 0;
 	ret
 */
 void delay_ms(unsigned int ms) {
-    unsigned int t = ms;
-    while (t > 750) {
-        ticks = 0;
-        while(ticks < 250);
-        t = t - 250;
-    }
-    if (t > 500) {
-        ticks = 0;
-        while(ticks < 250);
-        t = t - 250;
-    }
-    if (t > 250) {
-        ticks = 0;
-        while(ticks < 250);
-        t = t - 250;
-    }
-    ticks = 0;
-    while(ticks < t);
+	unsigned int t = ms;
+	while ( t > 750 ) {
+		ticks = 0;
+		while ( ticks < 250 )
+			;
+		t = t - 250;
+	}
+	if ( t > 500 ) {
+		ticks = 0;
+		while ( ticks < 250 )
+			;
+		t = t - 250;
+	}
+	if ( t > 250 ) {
+		ticks = 0;
+		while ( ticks < 250 )
+			;
+		t = t - 250;
+	}
+	ticks = 0;
+	while ( ticks < t )
+		;
 }
 
 void Timer_init() {
-    //PCON2 &= 0x00;
-    TMOD = 0x01;    /* Timer0 mode1 16-bit */
-    TH0 = 0xf4;     /* 1ms timer value for 12T @35MHz */
-    TL0 = 0x9b;
-    TR0 = 1;        /* Start timer0 */
+	// PCON2 &= 0x00;
+	TMOD = 0x01; /* Timer0 mode1 16-bit */
+	TH0 = 0xf4;  /* 1ms timer value for 12T @35MHz */
+	TL0 = 0x9b;
+	TR0 = 1; /* Start timer0 */
 }
 
-void Timer0_ISR() __interrupt (1)   /* Timer0 interrupt service routine (ISR) */
+void Timer0_ISR() __interrupt(1) /* Timer0 interrupt service routine (ISR) */
 {
-    ticks++;
-    TH0 = 0xf4;     /* 1ms timer value */
-    TL0 = 0x9b;
+	ticks++;
+	TH0 = 0xf4; /* 1ms timer value */
+	TL0 = 0x9b;
 }
 
 void main(void) {
-    EA  = 1;        /* Enable global interrupt */
-    ET0 = 1;        /* Enable timer0 interrupt */
-    Timer_init();
-    while(1) {
-        P3 ^= 0x40;
-        delay_ms(19);
-        P3 ^= 0x40;
-        delay_ms(1);
-    }
+	EA = 1;  /* Enable global interrupt */
+	ET0 = 1; /* Enable timer0 interrupt */
+	Timer_init();
+	while ( 1 ) {
+		P3 ^= 0x40;
+		delay_ms(19);
+		P3 ^= 0x40;
+		delay_ms(1);
+	}
 }
