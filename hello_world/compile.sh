168c13cbcb421737468ea0c6318569137e011028
Adam Allaf <adam.allaf@hotmail.com>
2024-07-27 16:42:32 +0200

    Reformatted hello_world

--- hello_world/compile.sh
+++ hello_world/compile.sh
@@ -1,4 +1,4 @@
 mkdir -p build/
 rm -vrf build/hello.*
 (cd build && sdcc -mmcs51 ../hello.c || exit)
-packihx build/hello.ihx > hello.hex
+packihx build/hello.ihx >hello.hex
--- hello_world/hello.c
+++ hello_world/hello.c
@@ -1,36 +1,40 @@
 #include <mcs51reg.h>
 
-
 char n = 0x40; // P3.6
 
 void delay(void);
 
 void main(void) {
-    while(1){
-        P3 |= n;
-        delay();  // 530ms delay
-        delay();  //
-        delay();  //
-        delay();  //
-        delay();  //
-        P3 &= ~n;
-        delay();  // 530ms delay
-        delay();  //
-        delay();  //
-        delay();  //
-        delay();  //
-    }
+	while ( 1 ) {
+		P3 |= n;
+		delay(); // 530ms delay
+		delay(); //
+		delay(); //
+		delay(); //
+		delay(); //
+		P3 &= ~n;
+		delay(); // 530ms delay
+		delay(); //
+		delay(); //
+		delay(); //
+		delay(); //
+	}
 }
 
 /*
  * ~106ms delay @11.234MHz
-*/
+ */
 void delay(void) {
-    int i, j, k, l, m, n;
-    for(i = 0; i < 0xff; i++)
-        for(j = 0; j < 0xff; j++);
-            for(k = 0; k < 0xff; k++);
-                for(l = 0; l < 0xff; l++);
-                    for(m = 0; m < 0xff; m++);
-                        for(n = 0; n < 0xff; n++);
+	int i, j, k, l, m, n;
+	for ( i = 0; i < 0xff; i++ )
+		for ( j = 0; j < 0xff; j++ )
+			;
+	for ( k = 0; k < 0xff; k++ )
+		;
+	for ( l = 0; l < 0xff; l++ )
+		;
+	for ( m = 0; m < 0xff; m++ )
+		;
+	for ( n = 0; n < 0xff; n++ )
+		;
 }
