8a3aa0311cfa6bc490deaa0671ddf93890657de5
Adam Allaf <adam.allaf@hotmail.com>
2024-07-27 16:44:31 +0200

    Reformatted isntall stcgal script

--- install_stcgal.sh
+++ install_stcgal.sh
@@ -6,6 +6,6 @@ python3.10 -m virtualenv --python=python3.10 "stcgal-pyenv" || (echo "[!] Couldn
 echo "[*] Created python virtualenv for stcgal"
 ./stcgal-pyenv/bin/pip install stcgal || (echo "[!] Couldn't install stcgal" && exit)
 echo "[*] stcgal installation successful"
-ln -s `pwd`/stcgal-pyenv/bin/stcgal ./stcgal || exit
+ln -s $(pwd)/stcgal-pyenv/bin/stcgal ./stcgal || exit
 echo "[*] stcgal shortcut created"
-echo `./stcgal -V`
+echo $(./stcgal -V)
